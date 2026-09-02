1f2d3b50e39a5883a90757d47ed951f684ec7f46
jordanosmorais <jordanaa003@gmail.com>
2026-09-02 17:54:13 -0300

    APRENDIZADO SOBRE VARIÁVEIS

---
+++ VARIAVEIS/src/main/java/com/mycompany/variaveis/VARIAVEIS.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+// comentario de uma unica linha
+/*comentario 
+em varias
+linhas;
+*/
+
+package com.mycompany.variaveis;
+
+/**
+ *
+ * @author JordanaLeite
+ */
+public class VARIAVEIS {
+// a classe main é quem executa os codigos
+    public static void main(String[] args) {// inicio da main
+   /*códigos de saida
+        - sao utilizados para imprimir ou mostrar dados e mensagens*/     
+    System.out.println("Nihao");
+    System.out.println("Eu sou uma programadora.");
+    
+    int idade = 23;
+    double altura = 1.68;
+    String nome = "Jordana";//decimais grandes
+    double peso = 65.5;
+    boolean solteira = true;
+    System.out.println ("Ola "+nome);
+    }// fim da main
+}
+// nenhum código pode ficar fora das chaves
